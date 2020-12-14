7b70e4ca55200a31270634c8cfb6674b3d02b12b
Pablo Mérida <pablomerida2001@gmail.com>
2020-12-14 08:45:52 +0100

    basic server

--- Server/SeverMultiSegSoc/src/Server/Main.java
+++ Server/SeverMultiSegSoc/src/Server/Main.java
@@ -1,9 +1,27 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
+	static ServerSocket server; 
+	static Socket socket;
+	static int port = 5000;
+	
 	public static void main(String[] args) {
-
+		try {
+			server = new ServerSocket(port);
+			while(true) {
+					socket = new Socket();		
+					System.out.println("Waiting connection...");
+					socket = server.accept();
+					ConnectionThread connection = new ConnectionThread(socket);
+					connection.start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
-
 }

7adfe21b7897c9b251cc5a315c10b06a4fa56ada
Vitaliy <vitaliygbbay@gmail,com>
2021-01-09 01:30:45 +0100

    Server-4- Receiving of unread email from Gmail

--- Server/SeverMultiSegSoc/src/Mailer/Mailer.java
+++ Server/SeverMultiSegSoc/src/Mailer/Mailer.java
@@ -7,13 +7,26 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.search.FlagTerm;
 
+
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
+import javax.mail.Flags;
+import javax.mail.Folder;
 import javax.mail.Message;  
 import javax.mail.Session;
+import javax.mail.Store;
 import javax.mail.Transport;
+
+import java.util.Arrays;
+
+
+import javax.mail.Address;
  
 
 public class Mailer {
@@ -45,7 +58,6 @@ public class Mailer {
         	return false;
         }    
 	}
-	
 	//Comprobar datos de usuario
 	private static Session getAuthentication(Properties properties, String user, String password) {
 		return Session.getDefaultInstance(properties,    
@@ -54,5 +66,74 @@ public class Mailer {
    	        return new PasswordAuthentication(user, password);
    	    }
    });  
+	}
+	
+	
+	/////////////////////////////
+	
+	public static void readInboundEmails(String host, String storeType, String user,
+		      String password) throws MessagingException{
+		Session session = Session.getDefaultInstance(new Properties( ));
+	    Store store = session.getStore("imaps");
+	    store.connect("imap.googlemail.com", 993, user, password);
+//	    store.connect(host, 993, user, password);
+	    Folder inbox = store.getFolder( "INBOX" );
+	    inbox.open( Folder.READ_ONLY );
+
+	    // Fetch unseen messages from inbox folder
+	    Message[] messages = inbox.search(
+	        new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+
+	    // Sort messages from recent to oldest
+	    Arrays.sort( messages, ( m1, m2 ) -> {
+	      try {
+	        return m2.getSentDate().compareTo( m1.getSentDate() );
+	      } catch ( MessagingException e ) {
+	        throw new RuntimeException( e );
+	      }
+	    } );
+
+	    for ( Message message : messages ) {
+	      try {
+			try {
+				System.out.println( 
+				      "sendDate: " + message.getSubject()
+				      + " subject:" + getTextFromMessage(message) + " " +
+				    		  ((InternetAddress)((Address)(message.getFrom()[0]))).getAddress());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    }
+	  }
+	private static String getTextFromMessage(Message message) throws MessagingException, IOException {
+	    String result = "";
+	    if (message.isMimeType("text/plain")) {
+	        result = message.getContent().toString();
+	    } else if (message.isMimeType("multipart/*")) {
+	        MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+	        result = getTextFromMimeMultipart(mimeMultipart);
+	    }
+	    return result;
+	}
+
+	private static String getTextFromMimeMultipart(
+	        MimeMultipart mimeMultipart)  throws MessagingException, IOException{
+	    String result = "";
+	    int count = mimeMultipart.getCount();
+	    for (int i = 0; i < count; i++) {
+	        BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+	        if (bodyPart.isMimeType("text/plain")) {
+	            result = result + "\n" + bodyPart.getContent();
+	            break; // without break same text appears twice in my tests
+	        } else if (bodyPart.getContent() instanceof MimeMultipart){
+	            result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+	        }
+	    }
+	    return result;
 	}
 }
--- Server/SeverMultiSegSoc/src/Server/Main.java
+++ Server/SeverMultiSegSoc/src/Server/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.mail.MessagingException;
+
 import Database.DbConnection;
 import Database.Services.UserService.FindUser;
 import Mailer.Mailer;
@@ -16,18 +18,31 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		DbConnection dbconnection = new DbConnection();
-		try {
-			server = new ServerSocket(port);
-			while(true) {
-					socket = new Socket();		
-					System.out.println("Waiting connection...");
-					socket = server.accept();
-					ConnectionThread connection = new ConnectionThread(socket, dbconnection);
-					connection.start();
-			}
-		} catch (IOException e) {
+		 String host = "pop.gmail.com";//change accordingly  
+		  String mailStoreType = "pop3";  
+		  String username= "vitaliygbbay@gmail.com";  
+		  String password= "vitvitvit";//change accordingly  
+		  
+//		  Mailer.receiveEmail(host, mailStoreType, username, password);  
+//		  Mailer.check(host, mailStoreType, username, password);  
+		  try {
+			Mailer.readInboundEmails(host, mailStoreType, username, password);
+		} catch (MessagingException e) {
 			e.printStackTrace();
-		}	
+		}  
+		
+//		DbConnection dbconnection = new DbConnection();
+//		try {
+//			server = new ServerSocket(port);
+//			while(true) {
+//					socket = new Socket();		
+//					System.out.println("Waiting connection...");
+//					socket = server.accept();
+//					ConnectionThread connection = new ConnectionThread(socket, dbconnection);
+//					connection.start();
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}	
 	}
 }
